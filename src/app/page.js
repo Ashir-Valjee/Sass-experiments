1170230fb84fcb8b41b7a8a0f42c811745a265f3
Ashir-Valjee <ashir@eco-motive.com>
2024-10-30 12:01:18 +0000

    added hamburger

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,15 @@
 // import "./home.modules.scss";
 import NewNavbar from "@/components/NewNavbar";
-// import "./style.scss";
+import "./style.scss";
 
 export default function HomePage() {
   return (
     <div>
-      <NewNavbar/>
-      <h1> Home Page</h1>
-      <p> This is a week 10 project week.</p>
+      <NewNavbar />
+      <div className="homeContent">
+        <h1> Home Page</h1>
+        <p> This is a week 10 project week.</p>
+      </div>
     </div>
   );
 }
--- src/components/NewNavbar.jsx
+++ src/components/NewNavbar.jsx
@@ -1,25 +1,50 @@
+"use client";
+
 import Link from "next/link";
 import "./_navstyles.scss";
 import Hamburger from "@/../public/hamburger.svg";
 import Image from "next/image";
+import { useState } from "react";
 
+export default function NewNavbar() {
+  const [showMenu, setShowMenu] = useState(true);
 
-export default function NewNavbar(){
-    return(
-        <div>
-        <button className="menu-button">
-            <Image src={Hamburger} alt="Hamburger" width={40} height={24}></Image>
-        </button>
+  function handleShow() {
+    setShowMenu(!showMenu);
+  }
+
+  return (
+    <div>
+      <button onClick={handleShow} className="menu-button">
+        <Image src={Hamburger} alt="Hamburger" width={40} height={24}></Image>
+      </button>
+
+      {showMenu ? (
         <nav className="main-nav">
-        <ul className="main-nav__list">
-        <li className="main-nav__item"> <Link href="/" className="main-nav__link"> Home</Link> </li>
-        <li className="main-nav__item"> <Link href="/" className="main-nav__link"> About</Link> </li>
-        <li className="main-nav__item"> <Link href="/" className="main-nav__link">Shop</Link></li>
-        
-        
-        
-        </ul>
+          <ul className="main-nav__list">
+            <li className="main-nav__item">
+              {" "}
+              <Link href="/" className="main-nav__link">
+                {" "}
+                Home
+              </Link>{" "}
+            </li>
+            <li className="main-nav__item">
+              {" "}
+              <Link href="/" className="main-nav__link">
+                {" "}
+                About
+              </Link>{" "}
+            </li>
+            <li className="main-nav__item">
+              {" "}
+              <Link href="/" className="main-nav__link">
+                Shop
+              </Link>
+            </li>
+          </ul>
         </nav>
-        </div>
-    );
+      ) : null}
+    </div>
+  );
 }
