51366312b8f2054c13282b6ad971ceaddaf54e5f
Ashir-Valjee <ashir@eco-motive.com>
2024-10-31 08:36:03 +0000

    added background image

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,27 @@
 // import "./home.modules.scss";
-import NewNavbar from "@/components/NewNavbar";
-import "./style.scss";
+
+import Image from "next/image";
+import myBackground from "@/../public/myBackground.png";
+import "./_homeStyles.scss";
+import styles from "./style.module.css";
 
 export default function HomePage() {
   return (
     <div>
-      <NewNavbar />
-      <div className="homeContent">
-        <h1> Home Page</h1>
-        <p> This is a week 10 project week.</p>
+      <Image
+        alt="background image"
+        src={myBackground}
+        layout="fill"
+        objectFit="cover"
+        quality={100}
+        className={styles.backgroundImage}
+      />
+      <div>
+        {/* <NewNavbar /> */}
+        <div className="homeContent">
+          <h1 className="homeContent__title"> Home Page</h1>
+          <p className="homeContent__text"> This is a week 10 project week.</p>
+        </div>
       </div>
     </div>
   );
